12a4d599a52175f1f706d0b5efed71095c2803b7
Sandroisu <sandroisu1411@gmail.com>
2025-09-29 21:43:04 +0300

    added serial name to dto classes

--- api/src/main/java/dev/sandroisu/aimobilelab/api/responses/Responses.kt
+++ api/src/main/java/dev/sandroisu/aimobilelab/api/responses/Responses.kt
@@ -1,39 +1,48 @@
 package dev.sandroisu.aimobilelab.api.responses
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class GmPart(
+    @SerialName("text")
     val text: String? = null,
 )
 
 @Serializable
 data class GmContent(
+    @SerialName("role")
     val role: String? = null,
+    @SerialName("parts")
     val parts: List<GmPart>,
 )
 
 @Serializable
 data class GmGenerateRequest(
+    @SerialName("contents")
     val contents: List<GmContent>,
 )
 
 @Serializable
 data class GmTextPart(
+    @SerialName("text")
     val text: String? = null,
 )
 
 @Serializable
 data class GmContentResp(
+    @SerialName("parts")
     val parts: List<GmTextPart>? = null,
 )
 
 @Serializable
 data class GmCandidate(
+    @SerialName("content")
     val content: GmContentResp? = null,
 )
 
 @Serializable
 data class GmGenerateResponse(
+    @SerialName("candidates")
     val candidates: List<GmCandidate>? = null,
 )
